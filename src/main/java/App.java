18c1fa06659ccac6c09efb78961d43b9b79b6663
alexmwaura <alexmwaura43@gmail.com>
2020-11-02 20:07:44 +0300

    add post method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,23 +1,34 @@
-import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
-import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.post;
+
 public class App {
     public static void main(String[] args) {
 
         get("/", (req, res) -> {
-            //just for testing - make two new objects so we have something to retrieve
-            Rectangle rectangle = new Rectangle(3,2);
-            Rectangle otherRectangle = new Rectangle(12, 12);
-
             Map<String, ArrayList<Rectangle>> model = new HashMap<>();
             ArrayList myRectangleArrayList = Rectangle.getAll();
             model.put("myRectangles", myRectangleArrayList);
             return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/rectangles/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>(); // I may be different from your code to account for generic “Objects”
+            int height = Integer.parseInt(req.queryParams("height"));
+            int width = Integer.parseInt(req.queryParams("width"));
+            Rectangle myRectangle = new Rectangle(height, width); //as we know this will automatically add itself to a list of all rectangles.
+            model.put("myRectangle", myRectangle); //don’t forget me!
+
+            if (myRectangle.getShape()) {
+                Cube myCube = new Cube(myRectangle);
+                model.put("myCube", myCube);
+            }
+            return new ModelAndView(model, "rectangle-details.hbs"); //render a detail page instead of redirecting to home.
         }, new HandlebarsTemplateEngine());
     }
 }
